98a23c6f4d473dd6a5598c2d70c6707f9d3d2620
MATTHIASW\MatthiasW <matthias.winkler94@gmail.com>
2017-06-23 13:39:11 +0200

    DB Änderung

--- model/db.js
+++ model/db.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/eventDb');
+mongoose.connect('mongodb://hias:hias1234@ds030500.mlab.com:30500/mtd');
